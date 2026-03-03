1e72b69dc19dc0eeee0d86e05bc4a52a571bc910
Csahoo009 <sahoochiranjeevi009@gmail.com>
2026-03-04 01:32:03 +0530

    UC13: Performance Comparison

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,15 +1,28 @@
 import java.util.Scanner;
-import java.util.Stack;
 
-interface PalindromeStrategy {
-    boolean check(String input);
-}
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-class TwoPointerStrategy implements PalindromeStrategy {
-    @Override
-    public boolean check(String input) {
+        System.out.print("Input : ");
+        String input = scanner.nextLine();
+
+        long startTime = System.nanoTime();
+        boolean isPalindrome = checkPalindrome(input);
+        long endTime = System.nanoTime();
+
+        long duration = endTime - startTime;
+
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + duration + " ns");
+
+        scanner.close();
+    }
+
+    public static boolean checkPalindrome(String input) {
         int start = 0;
         int end = input.length() - 1;
+
         while (start < end) {
             if (input.charAt(start) != input.charAt(end)) {
                 return false;
@@ -18,48 +31,5 @@ class TwoPointerStrategy implements PalindromeStrategy {
             end--;
         }
         return true;
-    }
-}
-
-class StackStrategy implements PalindromeStrategy {
-    @Override
-    public boolean check(String input) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-class PalindromeContext {
-    private PalindromeStrategy strategy;
-
-    public void setStrategy(PalindromeStrategy strategy) {
-        this.strategy = strategy;
-    }
-
-    public boolean executeCheck(String input) {
-        return strategy.check(input);
-    }
-}
-
-public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        PalindromeContext context = new PalindromeContext();
-
-        System.out.print("Input : ");
-        String input = scanner.nextLine();
-
-        context.setStrategy(new StackStrategy());
-        System.out.println("Is Palindrome? : " + context.executeCheck(input));
-
-        scanner.close();
     }
 }
