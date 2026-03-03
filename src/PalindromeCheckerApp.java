4c10701226273e6fc2a8445057ea3f81aeb39d7b
Csahoo009 <sahoochiranjeevi009@gmail.com>
2026-03-04 01:24:52 +0530

    UC12: Strategy Pattern for Palindrome Algorithms (Advanced)

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
+import java.util.Stack;
 
-class PalindromeService {
-    public boolean checkPalindrome(String input) {
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+class TwoPointerStrategy implements PalindromeStrategy {
+    @Override
+    public boolean check(String input) {
         int start = 0;
         int end = input.length() - 1;
-
         while (start < end) {
             if (input.charAt(start) != input.charAt(end)) {
                 return false;
@@ -16,16 +21,44 @@ class PalindromeService {
     }
 }
 
+class StackStrategy implements PalindromeStrategy {
+    @Override
+    public boolean check(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class PalindromeContext {
+    private PalindromeStrategy strategy;
+
+    public void setStrategy(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean executeCheck(String input) {
+        return strategy.check(input);
+    }
+}
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        PalindromeService service = new PalindromeService();
+        PalindromeContext context = new PalindromeContext();
 
         System.out.print("Input : ");
         String input = scanner.nextLine();
 
-        boolean isPalindrome = service.checkPalindrome(input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        context.setStrategy(new StackStrategy());
+        System.out.println("Is Palindrome? : " + context.executeCheck(input));
 
         scanner.close();
     }
